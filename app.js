3480d6dbb43969f6bee2627c68404259d568ac93
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2023-05-15 16:55:01 +0200

    mapa

--- app.js
+++ app.js
@@ -44,6 +44,22 @@ zoomed = map.fitBounds(circle1.getBounds());
 map.setView([lat, lng]);
 // Set map focus to current user position
 
+var tab1 = [[marker._latlng.lat, marker._latlng.lng], [marker1._latlng.lat, marker1._latlng.lng]]
+   var linia1 = L.polyline(tab1, {color: 'red'}).addTo(map)
+  
+   var latlngs = linia1.getLatLngs();
+   var lengthInMeters = 0;
+   for(var i=0; i<latlngs.length - 1; i++){
+    var latlng1 = latlngs[i];
+    var latlng2 = latlngs[i+1];
+    var distance = latlng1.distanceTo(latlng2);
+    lengthInMeters += distance;
+   }
+
+   var lengthInKilometers = lengthInMeters/1000;
+   var format = lengthInKilometers.toFixed(1)
+   linia1.bindPopup("Jesteś "+format+" kilometrów od szkoły.").openPopup()
+
 }
 
 function error(err) {
@@ -85,7 +101,7 @@ function onMapClick(e) {
    }
 
    var lengthInKilometers = lengthInMeters/1000;
-   var format = lengthInKilometers.toFixed(3)
+   var format = lengthInKilometers.toFixed(1)
    linia.bindPopup("Dystans to: "+format+" kilometrów.").openPopup()
 }
 map.on('click', onMapClick);
@@ -95,6 +111,7 @@ for(var i=0; i<=woje.features.length-1; i++){
 }
 wojew.on('click', wyswietl)
 
-function wyswietl(e){
-    console.log(e.layer.feature.properties.nazwa)
+function wyswietl(){
+    
+    console.log()
 }
